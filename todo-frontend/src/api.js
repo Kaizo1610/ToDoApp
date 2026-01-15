5b3e166326ff9f381e9e668df12939baa6f0781f
Your Name <your.email@example.com>
2026-01-16 03:29:22 +0800

    Doing Testing

--- todo-frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://localhost:5001/api/todos";
-
-// API functions
-export const getTodos = () => axios.get(API_URL);
-export const createTodo = (todo) => axios.post(API_URL, todo);
-export const updateTodo = (id, todo) => axios.put(`${API_URL}/${id}`, todo);
-export const deleteTodo = (id) => axios.delete(`${API_URL}/${id}`);
